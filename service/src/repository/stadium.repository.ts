1749f7c078e21b47dc3d4447d49e749010a4821a
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2021-03-25 20:15:20 -0300

    create basic methods for stadium.

--- service/src/repository/stadium.repository.ts
+++ service/src/repository/stadium.repository.ts
@@ -1,13 +1,21 @@
-import { getRepository, Repository } from "typeorm";
+import { DeleteResult, getRepository, Repository, UpdateResult } from "typeorm";
 import Stadium from "../models/stadium.model";
 
 export default class StadiumRepository extends Repository<Stadium>{
     private repository: Repository<Stadium>;
 
-    constructor(){
+    constructor() {
         super();
         this.repository = getRepository(Stadium);
     }
 
-    public store = async(data:Stadium): Promise<Stadium> => this.repository.save(data);
+    public store = async (data: Stadium): Promise<Stadium> => this.repository.save(data);
+
+    public list = async (): Promise<Stadium[]> => this.repository.find();
+
+    public show = async (id: number): Promise<Stadium> => this.repository.findOne({ where: { id } });
+
+    public updateData = async (data: Stadium): Promise<UpdateResult> => this.repository.update(data.id, data);
+
+    public removeData = async (id: number): Promise<DeleteResult> => this.repository.delete(id);
 }
