2a2606932643898bd8c9c1aa8fc7b246dcb2e0ec
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2021-03-25 20:24:35 -0300

    create routes of stadium

---
+++ service/src/routers/stadium.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import StadiumController from "../controllers/stadium.conroller";
+
+export default class StadiumRoutes {
+    private service: StadiumController;
+
+    constructor() {
+        this.service = new StadiumController();
+    }
+
+    public getRoutes = (): Router => {
+        const router = Router();
+        router.use('/stadium', router);
+        router.get('/', this.service.list);
+        router.post('/', this.service.store);
+        router.put('/', this.service.update);
+        router.get('/:id', this.service.show);
+        router.delete('/:id', this.service.remove);
+        return router;
+    }
+}
--- service/src/service/stadium.service.ts
+++ service/src/service/stadium.service.ts
@@ -9,4 +9,23 @@ export default class StadiumService{
     }
 
     public store = async(data: Stadium): Promise<Stadium> => this.repository.store(data);
+
+    public list = async(): Promise<Stadium[]> => this.repository.list();
+
+    public show = async (id: number): Promise<Stadium> => this.repository.show(id);
+
+    public update = async (data: Stadium): Promise<Stadium> => {
+        await this.repository.updateData(data);
+        return this.show(data.id);
+    };
+
+    public remove = async (id: number): Promise<string> => {
+        const object = await this.show(id);
+        if(object){
+            await this.repository.removeData(id);
+            return 'O estádio foi removido.'
+        }
+
+        return 'Não é possível remover um estádio que não se encontra na base de dados.';
+    };
 }
